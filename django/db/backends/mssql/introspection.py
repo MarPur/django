7e43f8699863d3e3b42e5c51c68a7f97c09f1f50
Martynas Puronas <martynas@puronas.me>
2020-01-28 22:57:29 +0000

    List tables in introspection

--- django/db/backends/mssql/introspection.py
+++ django/db/backends/mssql/introspection.py
@@ -4,4 +4,12 @@ from django.db.backends.base.introspection import (
 
 
 class DatabaseIntrospection(BaseDatabaseIntrospection):
-    pass
+
+    def get_table_list(self, cursor):
+        cursor.execute("""
+            SELECT TABLE_NAME, CASE WHEN TABLE_TYPE = 'BASE TABLE' THEN 't' ELSE 'v' END AS OBJECT_TYPE
+            FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_TYPE IN ('BASE TABLE', 'VIEW')
+        """)
+
+        return [TableInfo(row[0], row[1]) for row in cursor.fetchall()]
