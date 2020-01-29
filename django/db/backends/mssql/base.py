b33cfe51f10309da96b0052e5fb1abb5b874b779
Martynas Puronas <martynas@puronas.me>
2020-01-29 21:42:56 +0000

    Added data types

--- django/db/backends/mssql/base.py
+++ django/db/backends/mssql/base.py
@@ -7,6 +7,7 @@ from .creation import DatabaseCreation
 from .features import DatabaseFeatures
 from .introspection import DatabaseIntrospection
 from .operations import DatabaseOperations
+from .schema import DatabaseSchemaEditor
 
 
 class DatabaseWrapper(BaseDatabaseWrapper):
@@ -18,7 +19,38 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     creation_class = DatabaseCreation
     introspection_class = DatabaseIntrospection
     ops_class = DatabaseOperations
+
     Database = Database
+    SchemaEditorClass = DatabaseSchemaEditor
+
+    data_types = {
+        'AutoField': 'int, identity(1,1)',
+        'BigAutoField': 'bigint, identity(1,1)',
+        'BinaryField': 'varbinary(MAX)',
+        'BooleanField': 'bit',
+        'CharField': 'nvarchar(%(max_length)s)',
+        'DateField': 'date',
+        'DateTimeField': 'datetimeoffset', # Use simple datetime?
+        'DecimalField': 'decimal(%(max_digits)s, %(decimal_places)s)',
+        'DurationField': 'bigint',
+        'FileField': 'nvarchar(%(max_length)s)',
+        'FilePathField': 'nvarchar(%(max_length)s)',
+        'FloatField': 'double precision',
+        'IntegerField': 'real',
+        'BigIntegerField': 'bigint',
+        'GenericIPAddressField': 'varchar(39)',
+        'NullBooleanField': 'bit',
+        'OneToOneField': 'int',
+        'PositiveBigIntegerField': 'bigint',
+        'PositiveIntegerField': 'int',
+        'PositiveSmallIntegerField': 'smallint',
+        'SlugField': 'nvarchar(%(max_length)s)',
+        'SmallAutoField': 'smallint identity(1,1)',
+        'SmallIntegerField': 'smallint',
+        'TextField': 'nvarchar(max)',
+        'TimeField': 'time',
+        'UUIDField': 'char(32)',
+    }
 
     def get_connection_params(self):
         kwargs = {
