5ff180b12487f1e0917f41f20ecb44fcf9844914
Martynas Puronas <martynas@puronas.me>
2020-02-08 16:00:24 +0000

    Better support for datetime fields

--- django/db/backends/mssql/features.py
+++ django/db/backends/mssql/features.py
@@ -2,6 +2,7 @@ from django.db.backends.base.features import BaseDatabaseFeatures
 
 
 class DatabaseFeatures(BaseDatabaseFeatures):
+    supports_timezones = False
     requires_order_by_in_limit = True
     supports_transactions = True
     can_return_rows_from_bulk_insert = True
