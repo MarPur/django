9ec93a6a671d7b857801ce596733e19049367519
Martynas Puronas <martynas@puronas.me>
2020-02-09 11:19:21 +0000

    Datetime truncation

--- django/db/backends/mssql/schema.py
+++ django/db/backends/mssql/schema.py
@@ -4,7 +4,4 @@ from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
 
     def quote_value(self, value):
-        if isinstance(value, bool):
-            return "1" if value else "0"
-        else:
-            return str(value)
+        return str(value)
