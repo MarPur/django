2b330f757a2bf9de1044053e9fdd300c89b9ab74
Martynas Puronas <martynas@puronas.me>
2020-02-09 01:28:31 +0000

    fix SQL date extract for week day

--- django/db/backends/mssql/schema.py
+++ django/db/backends/mssql/schema.py
@@ -2,4 +2,9 @@ from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 
 
 class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
-    pass
+
+    def quote_value(self, value):
+        if isinstance(value, bool):
+            return "1" if value else "0"
+        else:
+            return str(value)
