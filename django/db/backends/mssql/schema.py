10b93484a462831018be5b8effaf25cd7bd4d270
Martynas Puronas <martynas@puronas.me>
2020-02-14 20:41:12 +0000

    SQL to drop and rename tables

--- django/db/backends/mssql/schema.py
+++ django/db/backends/mssql/schema.py
@@ -1,7 +1,24 @@
 from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 
+from django.db.backends.ddl_references import Statement
 
 class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
+    sql_rename_table = "EXEC sp_rename '%(old_table)s', '%(new_table)s'"
+    sql_delete_table = 'DROP TABLE %(table)s'
 
     def quote_value(self, value):
         return str(value)
+
+    def alter_db_table(self, model, old_db_table, new_db_table):
+        if (old_db_table == new_db_table or
+            (self.connection.features.ignores_table_name_case and
+                old_db_table.lower() == new_db_table.lower())):
+            return
+        self.execute(self.sql_rename_table % {
+            "old_table": old_db_table,
+            "new_table": new_db_table,
+        })
+
+        for sql in self.deferred_sql:
+            if isinstance(sql, Statement):
+                sql.rename_table_references(old_db_table, new_db_table)
