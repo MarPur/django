43c09b63fb46685e278ed003189d21e442dd5e57
Martynas Puronas <martynas@puronas.me>
2020-11-15 13:02:11 +0200

    Use RAND() for random ordering

--- django/db/models/functions/math.py
+++ django/db/models/functions/math.py
@@ -164,6 +164,9 @@ class Random(NumericOutputFieldMixin, Func):
     def as_mysql(self, compiler, connection, **extra_context):
         return super().as_sql(compiler, connection, function='RAND', **extra_context)
 
+    def as_mssql(self, compiler, connection, **extra_context):
+        return super().as_sql(compiler, connection, function='RAND', **extra_context)
+
     def as_oracle(self, compiler, connection, **extra_context):
         return super().as_sql(compiler, connection, function='DBMS_RANDOM.VALUE', **extra_context)
 
